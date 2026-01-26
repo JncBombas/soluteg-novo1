8b1d6e9f146415154e4e1f5608eebfb1c7be0174
JncBombas <soluteggeradores@gmail.com>
2026-01-25 23:23:45 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./oauth";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
-import { serveStatic, setupVite } from "../vite";
+import { serveStatic, setupVite } from "./vite";
 
 function isPortAvailable(port: number): Promise<boolean> {
   return new Promise(resolve => {
