7f43aacfd05040de66c707177f26ca6d1bbbf7df
JncBombas <soluteggeradores@gmail.com>
2026-03-08 22:13:37 -0300

    Atualização do sistema de mensgem via whats

--- server/whatsapp.ts
+++ server/whatsapp.ts
@@ -9,20 +9,37 @@ const client = new Client({
     }
 });
 
+// Variável para controlar se o Zap está pronto
+let isReady = false;
+
 client.on('qr', (qr) => {
-    console.log('--- LEIA O QR CODE PARA CONECTAR O ZAP DA JMC ---');
+    console.log('--- LEIA O QR CODE PARA CONECTAR O ZAP DA JNC ---');
     qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => console.log('✅ WHATSAPP DA JMC ELÉTRICA ON!'));
+client.on('ready', () => {
+    isReady = true;
+    console.log('✅ WHATSAPP DA JNC ELÉTRICA e BOMBAS ON!');
+});
+
+client.on('auth_failure', () => console.error('❌ Falha na autenticação do Zap!'));
+client.on('disconnected', () => {
+    isReady = false;
+    console.log('⚠️ Zap desconectado!');
+});
 
 client.initialize();
 
 export const sendWhatsappAlert = async (message: string) => {
+    if (!isReady) {
+        console.error('❌ Tentativa de envio falhou: O cliente WhatsApp não está pronto.');
+        return;
+    }
+
     try {
-        // Substitua pelo seu número (DDI + DDD + Número + @c.us)
         const meuNumero = "5513981301010@c.us"; 
         await client.sendMessage(meuNumero, message);
+        console.log('🚀 Mensagem enviada com sucesso para a JNC!');
     } catch (err) {
         console.error('Erro ao enviar Zap:', err);
     }
